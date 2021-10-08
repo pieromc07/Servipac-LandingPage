6d94727ae60654f5df8ebcc5f0ea74288613900d
Alexander Briones <alexanderbvart@gmail.com>
2021-10-08 13:25:39 -0500

    New feature	✨Header Component

---
+++ src/public/Components/Header.js
@@ -0,0 +1,24 @@
+const Header = ()=>{
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1><a href="#">cervipack</a></h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#Mision/">Misión y visión</a>
+            </div>
+            <div class="Header-nav">
+                <a href="#about/">About</a>
+            </div>
+            <div class="Header-nav">
+                <a href="#about/">About</a>
+            </div>
+            <div class="Header-nav">
+                <a href="#about/">About</a>
+            </div>
+            
+        </div>
+    `;
+    return view;
+};
+export default Header;
