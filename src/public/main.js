df05a9b23e3fc1aea16452905aac2afcf8742ef6
Alexander Briones <alexanderbvart@gmail.com>
2021-10-08 15:51:13 -0500

    starting point of the app

--- src/public/main.js
+++ src/public/main.js
@@ -1,11 +1,16 @@
-console.log("sdsadaws1")
 import Header from './Components/Header.js'
+import Footer from './Components/Footer.js' //componete
 const header = null || document.getElementById('Header');
 const banner = null || document.getElementById('Banner');
+const footer = null || document.getElementById('Footer'); //html
 
 const main = async () => {  
-    console.log("sdsadaws")
-    header.innerHTML = await Header();
+    header.innerHTML = Header();
+    var btnOpenPopup = document.getElementById('btn-open-popup')
+    btnOpenPopup.addEventListener('click', ()=>{
+        console.log("toogle")
+        header.classList.toggle('active');
+    })
+    footer.innerHTML = Footer();
 }
 main()
-/* export default main; */
